f589a47b9bd7f32260fe5664b1bc949e1018d02e
guerdani929 <GuerDani929@GoTVDSB.ca>
2020-10-27 13:36:32 -0400

    Add project files.

---
+++ Tut1/Game1.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+
+namespace Tut1
+
+    
+{
+
+    public class Game1 : Game
+
+    
+
+
+    {
+        private Texture2D background;
+        private Texture2D shuttle;
+        private Texture2D earth;
+
+        private SpriteFont font;
+        private int score = 0;
+
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch spriteBatch;
+
+        private KeyboardState oldState;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+
+            background = Content.Load<Texture2D>("stars"); // change these names to the names of your images
+            shuttle = Content.Load<Texture2D>("shuttle");  // if you are using your own images.
+            earth = Content.Load<Texture2D>("earth");
+
+            font = Content.Load<SpriteFont>("File"); // Use the name of your sprite font file here instead of 'Score'.
+
+
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+            score++;
+
+            KeyboardState newState = Keyboard.GetState();  // get the newest state
+
+            // handle the input
+            if (oldState.IsKeyUp(Keys.Left) && newState.IsKeyDown(Keys.Left))
+            {
+                // do something here
+                // this will only be called when the key if first pressed
+            }
+
+            oldState = newState;  // set the new state as the old state for next time
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+
+            spriteBatch.Begin();
+
+            spriteBatch.Draw(background, new Rectangle(0, 0, 800, 480), Color.White);
+            spriteBatch.Draw(earth, new Vector2(400, 240), Color.White);
+            spriteBatch.Draw(shuttle, new Vector2(450, 240), Color.White);
+
+            spriteBatch.DrawString(font, "Score: " + score, new Vector2(100, 100), Color.Blue);
+
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
